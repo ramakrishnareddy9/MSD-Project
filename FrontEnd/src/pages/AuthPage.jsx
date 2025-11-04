49c1c8a736c7409d433567c07d6dbb35b3e73392
RamaKrishna reddy <ramakrishnareddy.d9@gmail.com>
2025-11-04 19:03:24 +0530

    adding changes

--- FrontEnd/src/pages/AuthPage.jsx
+++ FrontEnd/src/pages/AuthPage.jsx
@@ -1,31 +1,12 @@
-import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { Container, Box, Tabs, Tab } from '@mui/material';
 import Login from './Login.jsx';
 import Signup from './Signup.jsx';
 
 const AuthPage = () => {
   const location = useLocation();
-  const [activeTab, setActiveTab] = useState(location.pathname === '/signup' ? 1 : 0);
-
-  const handleTabChange = (event, newValue) => {
-    setActiveTab(newValue);
-  };
-
-  return (
-    <Container maxWidth="lg">
-      <Box sx={{ minHeight: '90vh', py: 4 }}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider', mb: 3 }}>
-          <Tabs value={activeTab} onChange={handleTabChange} centered>
-            <Tab label="Login" />
-            <Tab label="Sign Up" />
-          </Tabs>
-        </Box>
-        
-        {activeTab === 0 ? <Login /> : <Signup />}
-      </Box>
-    </Container>
-  );
+  
+  // Simply show Login or Signup based on the route - no tabs, no changes
+  return location.pathname === '/signup' ? <Signup /> : <Login />;
 };
 
 export default AuthPage;
